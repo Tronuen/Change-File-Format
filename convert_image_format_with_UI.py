bc5c2a29a72aa176b1219a243cceb1d4655c4252
Tronuen <aziz.2318@gmail.com>
2022-09-27 10:25:15 +0300

    Update convert_image_format_with_UI.py

--- convert_image_format_with_UI.py
+++ convert_image_format_with_UI.py
@@ -1,20 +1,31 @@
 from tkinter import *
-from tkinter import messagebox
 from tkinter.ttk import Combobox
 from PIL import Image
 from os import listdir as ld
 import os
 
 
+get_from_folder = os.environ['HOMEPATH'] + '\Desktop\convert\\'
+put_to_folder = os.environ['HOMEPATH'] + '\Desktop\converted\\'
+
+
+def convert_all(to_ext, mode):
+    for file in ld(get_from_folder):
+        x, from_ext = os.path.splitext(file)
+        im = Image.open(get_from_folder + file).convert("RGB")
+        im.save(put_to_folder + file.replace(from_ext, to_ext), mode)
+
 def convert_fonk():
-    get_from_folder = os.environ['HOMEPATH'] + '\Desktop\convert\\'
-    put_to_folder = os.environ['HOMEPATH'] + '\Desktop\converted\\'
+    from_ext = "." + cb1.get()
+    to_ext = "." + cb2.get()
     
-    from_ext = data1.get()
-    to_ext = data2.get()
     mode = to_ext[1:]
     if to_ext==".jpg":
         mode = "jpeg"
+        
+    if from_ext==".all":
+        convert_all(to_ext, mode)
+        return
     
     files = [file for file in ld(get_from_folder) if file.endswith(from_ext)]
 
@@ -35,14 +46,19 @@ title.place(x = 130, y = 10)
 txt1 = Label(text = "From", fg = "#08cfdd", bg = "white", font = ("Open Sans", "13", "bold"))
 txt1.place(x = 70, y = 70)
 
-data1 = Entry(bd = 2, font = ("Times", "14", "normal"))
-data1.place(x = 130, y = 70, width = 70, height = 30)
+extensions = ['all', 'jpg', 'jpeg', 'png', 'webp']
+var1 = StringVar()
+var1.set("all")
+cb1 = Combobox(master = window, textvariable = var1, values = extensions)
+cb1.place(x=130, y=70, width=70, height=30)
 
 txt2 = Label(text = "To", fg = "#08cfdd", bg = "white", font = ("Open Sans", "13", "bold"))
 txt2.place(x = 260, y = 70)
 
-data2 = Entry(bd = 2, font = ("Times", "14", "normal"))
-data2.place(x = 300, y = 70, width = 70, height = 30)
+var2 = StringVar()
+var2.set("jpg")
+cb2 = Combobox(master = window, textvariable = var2, values = extensions[1:])
+cb2.place(x=300, y=70, width=70, height=30)
 
 buton_cnvrt = Button(text = "Convert", activeforeground = "white", activebackground = "#7fd6db", command = convert_fonk)
 buton_cnvrt.place(x = 180, y = 150, width = 100, height = 50)
